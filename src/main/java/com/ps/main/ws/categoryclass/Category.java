87f7e69f32ae8e210f1b5a8ed027c1ac571669eb
mohamedhabila <mohammedhabila@gmail.com>
2024-06-02 19:49:20 +0200

    Update Category model to add description

--- src/main/java/com/ps/main/ws/categoryclass/Category.java
+++ src/main/java/com/ps/main/ws/categoryclass/Category.java
@@ -10,17 +10,19 @@ public class Category {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
-	
+
 	String name;
+	String description;
 
 	public Category() {
 		
 	}
 
-	public Category(Integer id, String name) {
+	public Category(Integer id, String name, String description) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
 	}
 
 	public Integer getId() {
@@ -37,6 +39,14 @@ public class Category {
 
 	public void setName(String name) {
 		this.name = name;
-	}	
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	
 }
